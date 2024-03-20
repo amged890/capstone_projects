0a38a93f730f5c0966a0c638d9380b9345b93b05
amged890 <153000606+amged890@users.noreply.github.com>
2024-03-20 21:45:53 +0000

    Create finance_calculators.py
    created a code for calculating investments and mortgages

---
+++ finance_calculators.py
@@ -0,0 +1,42 @@
+# This code is a financial calculator for investments and home loan repayments . We ask the user
+# what they want, and we proceed according to their choice. If they choose investments we ask them for the initial
+# amount, time in years, rate per year and investment type. If they choose bond we ask about the house value,
+# rate per year and time in months and return the amount they should pay each month.
+
+import math
+
+print("investment    - to calculate the amount of interest you'll earn on your investment.")
+print("bond          - to calculate the amount you'll have to pay on a home loan.")
+
+calculator_type = (input("Enter either 'investment' or 'bond' from the menu above to proceed:  ")).lower()
+
+
+if calculator_type == "investment":
+    deposit = int(input("Enter the amount of money you're depositing: "))
+    interest_rate = (float(input("Enter the interest rate per year: %"))) / 100
+    time_years = int(input("Enter the number of years you plan to invest: "))
+    interest = (input("There's two interest types 'simple' and 'compound'. Enter the type you want: ")).lower()
+
+    if interest == "simple":
+        total_amount = deposit * (1 + interest_rate * time_years)
+        print(f"The total amount you will get is {total_amount}£.")
+
+    elif interest == "compound":
+        total_amount = deposit * math.pow((1+interest_rate), time_years)
+        print(f"The total amount you will get is {total_amount}£.")
+
+    else:
+        print("Wrong input, please try again!")
+
+
+elif calculator_type == "bond":
+    house_value = int(input("Enter the present value of the house: "))
+    monthly_interest_rate = (float(input("Enter the interest rate per year: %"))) / 1200
+    time_month = int(input("Enter the number of months you plan to take to repay the bond: "))
+
+    repayment = (monthly_interest_rate * house_value) / (1 - (1 + monthly_interest_rate)**(-time_month))
+    print(f"The amount you have to repay each month is {repayment}£.")
+
+
+else:
+    print("Sorry, please try again with a valid choice from the menu.")
